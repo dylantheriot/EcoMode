c8493b3f9a21d33ef9323f627f5309a76995d5f4
Adel Hassan <sean.dashals@gmail.com>
2020-01-25 17:57:57 -0600

    Added rough bounding box

--- Frontend/src/ObjectsScreen.tsx
+++ Frontend/src/ObjectsScreen.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { TouchableOpacity, ImageBackground } from 'react-native';
 import { useGlobal } from 'reactn';
 import { State } from 'reactn/default';
 
 const ObjectsScreen = () => {
   const [imgURI, setImgURI] = useGlobal('imgURI');
   return (
-    <Image source={{uri: imgURI}} style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-    }}>
-      
-    </Image>
+    <ImageBackground source={{uri: imgURI}} style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}>
+      <TouchableOpacity style={{
+        position: 'absolute',
+        top: 100,
+        left: 50,
+        height: 250,
+        width: 200,
+        borderColor: 'blue',
+        borderWidth: 5,
+        borderStyle: 'solid',
+      }} />
+    </ImageBackground>
   );
 }
 
