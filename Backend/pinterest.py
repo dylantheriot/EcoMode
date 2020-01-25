10f56d8b8238ef1d813bb9930a1ce94eb4f2d5d5
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 17:33:57 -0600

    pinterest webscraper

---
+++ Backend/pinterest.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+def getPinterestInfo(query):
+    query = query.strip().replace(" ", "+")
+    query = 'https://www.bing.com/images/search?q=site:pinterest.com+' + query + "+diy+art"
+
+    # open the page and store it
+    my_url =  query
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+
+    body_page = page_soup.find("div", {"id":"b_content"})
+    results = body_page.findAll("div", {"class":"item"})
+    images = []
+    titles = []
+    links = []
+    for result in results:
+        temp = result.find("div", {"class":"meta"})
+        links.append(temp.a['href'])
+        titles.append(temp.div.text)
+        images.append(result.a['href'])
+    print(images)
+    print(titles)
+    print(links)
+
+getPinterestInfo('apple charger')
