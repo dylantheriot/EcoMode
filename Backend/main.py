1209ff2694c99db4baa1ad5868e25f7c2936817c
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 21:55:25 -0600

    yes

--- Backend/main.py
+++ Backend/main.py
@@ -1,46 +1,9 @@
 from flask import Flask
 from flask import jsonify
-from bs4 import BeautifulSoup as bs
 from pinterest import getPinterestInfo
-# from youtube import getYtInfo
+from youtube import getYtInfo
 # from location import getLocInfo
 from flask import request
-# import requests
-
-# def getYtInfo(itemDetected):
-
-#     URL = "https://www.youtube.com/results?search_query=recycling+" + itemDetected + "+ideas"
-
-#     response = requests.get(URL)
-#     page = response.text
-
-#     soup = bs(page, 'html.parser')
-
-#     videos = soup.findAll('a', class_ = "yt-uix-tile-link")
-
-#     titles = []
-#     links = []
-#     thumbnails = []
-
-#     for v in videos:
-#         href = v.get("href")
-#         thumb = href[9:]
-#         titles.append(v.get("title"))
-#         links.append("https://www.youtube.com" + href)
-#         thumbnails.append("https://i.ytimg.com/vi/"+ thumb + "/0.jpg")
-
-
-#     jsonOut = []
-
-#     for i in range(len(titles)):
-#         temp = {}
-#         temp['title'] = titles[i]
-#         temp['link'] = links[i]
-#         temp['thumbnail'] = thumbnails[i]
-#         jsonOut.append(temp)
-
-#     return jsonOut
-
 
 app = Flask(__name__)
 
@@ -54,11 +17,11 @@ def pinterest():
     list = getPinterestInfo(query)
     return jsonify(results = list)
 
-# @app.route("/youtube")
-# def youtube():
-#     query = request.args.get('q')
-#     list = getYtInfo(query)
-#     return jsonify(results = list)
+@app.route("/youtube")
+def youtube():
+    query = request.args.get('q')
+    list = getYtInfo(query)
+    return jsonify(results = list)
     
 # @app.route("/location")
 # def location():
--- Backend/youtube.py
+++ Backend/youtube.py
@@ -1,38 +1,37 @@
-# from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as soup
 import requests
 import json
 
 def getYtInfo(itemDetected):
 
-    # # URL = "https://www.youtube.com/results?search_query=recycling+" + itemDetected + "+ideas"
+    URL = "https://www.youtube.com/results?search_query=recycling+" + itemDetected + "+ideas"
 
-    # # response = requests.get(URL)
-    # # page = response.text
+    response = requests.get(URL)
+    page = response.text
 
-    # # soup = bs(page, 'html.parser')
+    soup1 = soup(page, 'html.parser')
 
-    # # videos = soup.findAll('a', class_ = "yt-uix-tile-link")
+    videos = soup1.findAll('a', class_ = "yt-uix-tile-link")
 
-    # titles = []
-    # links = []
-    # thumbnails = []
+    titles = []
+    links = []
+    thumbnails = []
 
-    # for v in videos:
-    #     href = v.get("href")
-    #     thumb = href[9:]
-    #     # title = v.get("title")
-    #     titles.append(v.get("title"))
-    #     links.append("https://www.youtube.com" + href)
-    #     thumbnails.append("https://i.ytimg.com/vi/"+ thumb + "/0.jpg")
+    for v in videos:
+        href = v.get("href")
+        thumb = href[9:]
+        titles.append(v.get("title"))
+        links.append("https://www.youtube.com" + href)
+        thumbnails.append("https://i.ytimg.com/vi/"+ thumb + "/0.jpg")
 
 
     jsonOut = []
 
-    # for i in range(len(titles)):
-    #     temp = {}
-    #     temp['title'] = titles[i]
-    #     temp['link'] = links[i]
-    #     temp['thumbnail'] = thumbnails[i]
-    #     jsonOut.append(temp)
+    for i in range(len(titles)):
+        temp = {}
+        temp['title'] = titles[i]
+        temp['link'] = links[i]
+        temp['thumbnail'] = thumbnails[i]
+        jsonOut.append(temp)
 
     return jsonOut
