81e1c1bb87f5a3a3f3d4e910513a823650a1ed4f
Adel Hassan <sean.dashals@gmail.com>
2020-01-25 20:31:24 -0600

    Added identify endpoint

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,10 @@ from flask import jsonify
 from pinterest import getPinterestInfo
 from youtube import getYtInfo
 from flask import request
+import os
+import errors
+import requests
+import base64
 
 app = Flask(__name__)
 
@@ -21,6 +25,26 @@ def youtube():
     query = request.args.get('q')
     list = getYtInfo(query)
     return jsonify(results = list)
+
+@app.route("/identify", methods=['POST'])
+def identify():
+    if os.getenv('AZURE_CV_KEY') is None:
+        raise errors.InternalError('The actual monkeys on the dev team didn\'t set an API key')
+
+    api_key = os.getenv('AZURE_CV_KEY')
+    endpoint = 'https://ecomode.cognitiveservices.azure.com/vision/v2.1/analyze'
+
+    img_bytes = base64.b64decode(request.data)
+    headers = {
+        'Ocp-Apim-Subscription-Key': api_key,
+        'Content-Type': 'application/octet-stream'
+    }
+    params = {'visualFeatures': 'Categories,Description,Color'}
+    response = requests.post(endpoint, headers=headers, params=params, data=img_bytes)
+    response.raise_for_status()
+    analysis = response.json()
+    return analysis
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
