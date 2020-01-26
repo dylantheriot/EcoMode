8c7d811ad2fcbf14f7e8a61bd862cf32298d0ad9
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 19:24:18 -0600

    updated gcloud info

--- Backend/main.py
+++ Backend/main.py
@@ -30,4 +30,4 @@ def location():
     return jsonify(results = list)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
