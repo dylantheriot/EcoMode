adc1310201d49e420da86b6bd2fa40f06418faa2
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 22:28:04 -0600

    deleted

--- backend/errors.py
+++
@@ -1,9 +0,0 @@
-class InternalError(Exception):
-    status_code = 500
-
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-    def to_dict(self):
-        return {'message': self.message}
