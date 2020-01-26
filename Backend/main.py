0e0537cd4e8e32aed8083798cffe0fe42ae53a91
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 21:48:08 -0600

    1

--- Backend/main.py
+++ Backend/main.py
@@ -1,45 +1,45 @@
 from flask import Flask
 from flask import jsonify
 from bs4 import BeautifulSoup as bs
-# from pinterest import getPinterestInfo
+from pinterest import getPinterestInfo
 # from youtube import getYtInfo
 # from location import getLocInfo
 from flask import request
-import requests
+# import requests
 
-def getYtInfo(itemDetected):
+# def getYtInfo(itemDetected):
 
-    URL = "https://www.youtube.com/results?search_query=recycling+" + itemDetected + "+ideas"
+#     URL = "https://www.youtube.com/results?search_query=recycling+" + itemDetected + "+ideas"
 
-    response = requests.get(URL)
-    page = response.text
+#     response = requests.get(URL)
+#     page = response.text
 
-    soup = bs(page, 'html.parser')
+#     soup = bs(page, 'html.parser')
 
-    videos = soup.findAll('a', class_ = "yt-uix-tile-link")
+#     videos = soup.findAll('a', class_ = "yt-uix-tile-link")
 
-    titles = []
-    links = []
-    thumbnails = []
+#     titles = []
+#     links = []
+#     thumbnails = []
 
-    for v in videos:
-        href = v.get("href")
-        thumb = href[9:]
-        titles.append(v.get("title"))
-        links.append("https://www.youtube.com" + href)
-        thumbnails.append("https://i.ytimg.com/vi/"+ thumb + "/0.jpg")
+#     for v in videos:
+#         href = v.get("href")
+#         thumb = href[9:]
+#         titles.append(v.get("title"))
+#         links.append("https://www.youtube.com" + href)
+#         thumbnails.append("https://i.ytimg.com/vi/"+ thumb + "/0.jpg")
 
 
-    jsonOut = []
+#     jsonOut = []
 
-    for i in range(len(titles)):
-        temp = {}
-        temp['title'] = titles[i]
-        temp['link'] = links[i]
-        temp['thumbnail'] = thumbnails[i]
-        jsonOut.append(temp)
+#     for i in range(len(titles)):
+#         temp = {}
+#         temp['title'] = titles[i]
+#         temp['link'] = links[i]
+#         temp['thumbnail'] = thumbnails[i]
+#         jsonOut.append(temp)
 
-    return jsonOut
+#     return jsonOut
 
 
 app = Flask(__name__)
@@ -48,17 +48,17 @@ app = Flask(__name__)
 def home():
     return('Howdy and welcome to the API for Eco-Mode! Please enjoy your stay.')
 
-# @app.route("/pinterest")
-# def pinterest():
-#     query = request.args.get('q')
-#     list = getPinterestInfo(query)
-#     return jsonify(results = list)
-
-@app.route("/youtube")
-def youtube():
+@app.route("/pinterest")
+def pinterest():
     query = request.args.get('q')
-    list = getYtInfo(query)
+    list = getPinterestInfo(query)
     return jsonify(results = list)
+
+# @app.route("/youtube")
+# def youtube():
+#     query = request.args.get('q')
+#     list = getYtInfo(query)
+#     return jsonify(results = list)
     
 # @app.route("/location")
 # def location():
