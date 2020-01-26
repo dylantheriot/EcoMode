5c77547b34d10c8983817fadb0d7112b9eca0031
Dylan Theriot <dylantheriot@gmail.com>
2020-01-25 19:58:33 -0600

    test

--- Backend/main.py
+++ Backend/main.py
@@ -1,33 +1,44 @@
-from flask import Flask
-from flask import jsonify
-from pinterest import getPinterestInfo
-from youtube import getYtInfo
-from location import getLocInfo
-from flask import request
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_render_template]
+import datetime
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return('Howdy and welcome to the API for Eco-Mode! Please enjoy your stay.')
 
-@app.route("/pinterest")
-def pinterest():
-    query = request.args.get('q')
-    list = getPinterestInfo(query)
-    return jsonify(results = list)
+@app.route('/')
+def root():
+    # For the sake of example, use static information to inflate the template.
+    # This will be replaced with real information in later steps.
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
-@app.route("/youtube")
-def youtube():
-    query = request.args.get('q')
-    list = getYtInfo(query)
-    return jsonify(results = list)
-    
-@app.route("/location")
-def location():
-    query = request.args.get('q')
-    list = getLocInfo(query)
-    return jsonify(results = list)
+    return render_template('index.html', times=dummy_times)
+
 
 if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+# [START gae_python37_render_template]
